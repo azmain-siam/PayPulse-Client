431d15f200beb371759edeee1ab16d5aeb6841c4
azmain-siam <siamhossain2700@gmail.com>
2024-07-17 11:57:25 +0600

    Send Money implemented

--- src/pages/SendMoney.jsx
+++ src/pages/SendMoney.jsx
@@ -7,6 +7,18 @@ const SendMoney = () => {
   const { user: userEmail } = useContextProvider();
   const axiosSecure = useAxiosSecure();
   const [error, setError] = useState("");
+  const [fee, setFee] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleTotalFee = (e) => {
+    const value = parseInt(e.target.value);
+    if (value > 99) {
+      setFee(5);
+    } else {
+      setFee(0);
+    }
+    setTotal(value);
+  };
 
   const handleSend = async (e) => {
     e.preventDefault();
@@ -37,60 +49,64 @@ const SendMoney = () => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Send Money
               </h1>
-              <form
-                onSubmit={handleSend}
-                className="space-y-4 md:space-y-6"
-                action="#"
-              >
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Recipient Phone Number
-                  </label>
-                  <input
-                    type="text"
-                    name="recipient_number"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Phone"
-                    required=""
-                  />
+
+              <form onSubmit={handleSend} className="" action="#">
+                <div className="space-y-4 md:space-y-6">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Recipient Phone Number
+                    </label>
+                    <input
+                      type="text"
+                      name="recipient_number"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Phone"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="amount"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Amount
+                    </label>
+                    <input
+                      onChange={handleTotalFee}
+                      type="number"
+                      name="send_amount"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Enter amount"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="pin"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your PIN
+                    </label>
+                    <input
+                      type="password"
+                      name="pin"
+                      id="password"
+                      placeholder="Enter the 5 digit PIN"
+                      maxLength={5}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                    {error && (
+                      <p className="text-red-600 text-sm mt-2">{error}</p>
+                    )}
+                  </div>
                 </div>
-                <div>
-                  <label
-                    htmlFor="amount"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Amount
-                  </label>
-                  <input
-                    type="number"
-                    name="send_amount"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Enter amount"
-                    required=""
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="pin"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your PIN
-                  </label>
-                  <input
-                    type="password"
-                    name="pin"
-                    id="password"
-                    placeholder="Enter the 5 digit PIN"
-                    maxLength={5}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                  />
-                  {error && (
-                    <p className="text-red-600 text-sm mt-2">{error}</p>
-                  )}
+                <div className="space-y-2 my-5 text-sm font-medium">
+                  <h1 className="">Fee: {fee}</h1>
+                  <h1 className="">Total Amount: {total + fee}</h1>
                 </div>
                 <button
                   type="submit"
@@ -98,7 +114,7 @@ const SendMoney = () => {
                 >
                   Send
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400 mt-4">
                   Don’t have an account yet?{" "}
                   <a
                     href="#"
